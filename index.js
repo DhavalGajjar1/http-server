ebd198a8380767ed937e796398888cb6967dfc97
DhavalGajjar1 <dhaval10gajjar@gmail.com>
2024-09-10 15:12:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require('fs');
+const port = 8082;
+
+const requestHeandle = (req, res) => {
+    console.log(req.url);
+    let fileName = "";
+    switch (req.url) {
+        case '/':
+            fileName = "index.html";
+            break;
+        case '/home':
+            fileName = "home.html";
+            break;
+        case '/about':
+            fileName = "about.html";
+            break;
+        default:
+            fileName = "error.html";
+    }
+
+    fs.readFile(fileName, (err, result) => {
+        if (!err) {
+            res.end(result);
+        }
+    });
+}
+
+const server = http.createServer(requestHeandle);
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log("Server not start on port..");
+        return false;
+    }
+    console.log("Server start on port:- " + port);
+});
